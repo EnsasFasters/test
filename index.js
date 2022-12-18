1f8c56498711b34a2020a7e83e6aa7df3fae40bd
EnsasFasters <120198765+EnsasFasters@users.noreply.github.com>
2022-12-18 16:35:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,63 @@ function NavBarPhone(){
    }
     sNavBarPhoneCounter++;
    }
+
+$('#activitsInner1').hover(function(){
+           $('#activitsInner2-1').css('top','70%');
+           $('#activitsInner2-1').css('color','var(--fire--)');
+      }
+    ,function(){
+             $('#activitsInner2-1').css('top','100%');
+             $('#activitsInner2-1').css('color','white');
+  }
+  );
+
+  $('#activitsInner2').hover(function(){
+           $('#activitsInner2-2').css('top','70%');
+           $('#activitsInner2-2').css('color','var(--fire--)');
+      }
+    ,function(){
+             $('#activitsInner2-2').css('top','100%');
+             $('#activitsInner2-2').css('color','white');
+  }
+  );
+
+  $('#activitsInner3').hover(function(){
+           $('#activitsInner2-3').css('top','70%');
+           $('#activitsInner2-3').css('color','var(--fire--)');
+      }
+    ,function(){
+             $('#activitsInner2-3').css('top','100%');
+             $('#activitsInner2-3').css('color','white');
+  }
+  );
+
+  $('#activitsInner4').hover(function(){
+           $('#activitsInner2-4').css('top','70%');
+           $('#activitsInner2-4').css('color','var(--fire--)');
+      }
+    ,function(){
+             $('#activitsInner2-4').css('top','100%');
+             $('#activitsInner2-4').css('color','white');
+  }
+  );
+     
+     $('#activitsInner5').hover(function(){
+           $('#activitsInner2-5').css('top','70%');
+           $('#activitsInner2-5').css('color','var(--fire--)');
+      }
+    ,function(){
+             $('#activitsInner2-5').css('top','100%');
+             $('#activitsInner2-5').css('color','white');
+  }
+  );
+
+   $('#activitsInner6').hover(function(){
+           $('#activitsInner2-6').css('top','70%');
+           $('#activitsInner2-6').css('color','var(--fire--)');
+      }
+    ,function(){
+             $('#activitsInner2-6').css('top','100%');
+             $('#activitsInner2-6').css('color','white');
+  }
+  );
