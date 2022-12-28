6c88132a33ae0438b2aaef51c96a644b95c71edf
EnsasFasters <120198765+EnsasFasters@users.noreply.github.com>
2022-12-28 15:29:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ $('headerNavBar')
 
 $('#activitsInner1').hover(function(){
            $('#activitsInner2-1').css('top','70%');
-           $('#activitsInner2-1').css('color','var(--fire--)');
+           $('#activitsInner2-1').css('color','yellow');
       }
     ,function(){
              $('#activitsInner2-1').css('top','100%');
@@ -22,7 +22,7 @@ $('#activitsInner1').hover(function(){
 
   $('#activitsInner2').hover(function(){
            $('#activitsInner2-2').css('top','70%');
-           $('#activitsInner2-2').css('color','var(--fire--)');
+           $('#activitsInner2-2').css('color','yellow');
       }
     ,function(){
              $('#activitsInner2-2').css('top','100%');
@@ -32,7 +32,7 @@ $('#activitsInner1').hover(function(){
 
   $('#activitsInner3').hover(function(){
            $('#activitsInner2-3').css('top','70%');
-           $('#activitsInner2-3').css('color','var(--fire--)');
+           $('#activitsInner2-3').css('color','yellow');
       }
     ,function(){
              $('#activitsInner2-3').css('top','100%');
@@ -42,7 +42,7 @@ $('#activitsInner1').hover(function(){
 
   $('#activitsInner4').hover(function(){
            $('#activitsInner2-4').css('top','70%');
-           $('#activitsInner2-4').css('color','var(--fire--)');
+           $('#activitsInner2-4').css('color','yellow');
       }
     ,function(){
              $('#activitsInner2-4').css('top','100%');
@@ -52,7 +52,7 @@ $('#activitsInner1').hover(function(){
      
      $('#activitsInner5').hover(function(){
            $('#activitsInner2-5').css('top','70%');
-           $('#activitsInner2-5').css('color','var(--fire--)');
+           $('#activitsInner2-5').css('color','yellow');
       }
     ,function(){
              $('#activitsInner2-5').css('top','100%');
@@ -61,11 +61,45 @@ $('#activitsInner1').hover(function(){
   );
 
    $('#activitsInner6').hover(function(){
-           $('#activitsInner2-6').css('top','70%');
-           $('#activitsInner2-6').css('color','var(--fire--)');
+           $('#activitsInner2-6').css({'top':'70%',});       //.css({ "background-color": "#ffe", "border-left": "5px solid #ccc" }) 
+           $('#activitsInner2-6').css('color','yellow');
       }
     ,function(){
              $('#activitsInner2-6').css('top','100%');
              $('#activitsInner2-6').css('color','white');
   }
   );
+//Benefits
+   $('#benefitsInner1').hover(
+       function() {
+          $('#benefitsInner1b').css('filter','blur(8px)');
+          $('#benefitsInner11').css('top','-60%');
+       }
+      ,function(){
+          $('#benefitsInner1b').css('filter','blur(0)');
+          $('#benefitsInner11').css('top','0');
+         })  
+   $('#benefitsInner2').hover(
+       function() {
+          $('#benefitsInner2b').css('filter','blur(8px)');
+          $('#benefitsInner22').css('top','-60%');
+       }
+      ,function(){ 
+          $('#benefitsInner2b').css('filter','blur(0)');
+          $('#benefitsInner22').css('top','0');
+         })
+   $('#benefitsInner3').hover(
+       function() {
+          $('#benefitsInner3b').css('filter','blur(8px)');
+          $('#benefitsInner33').css('top','-60%');
+       }
+      ,function(){
+          $('#benefitsInner3b').css('filter','blur(0px)');
+          $('#benefitsInner33').css('top', '0');
+      })
+
+$('.clicky').click(function(){
+if($(window).width() <= 570){                           
+ $('#slideNavBarButton').trigger('click');
+
+}});
